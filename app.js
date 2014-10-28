54161b2ca6bfddfe582e1ad1835cd5ec11c12b99
mooveprince <mooveprince@gmail.com>
2014-10-27 21:00:39 -0500

    db

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', routes);
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM userlist', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
